819abfac4816923cde16cf70a80bb2a1ca9528f8
TheAlternateDoctor <thealternatedoc@gmail.com>
2024-03-07 22:16:19 +0100

    Diffs can now be made

--- src/diff_utils.rs
+++ src/diff_utils.rs
@@ -1,21 +1,41 @@
 use crate::msbt::MSBTString;
 
-pub fn get_added(original: Vec<MSBTString>, edited: Vec<MSBTString>) -> Vec<MSBTString> {
+pub fn get_added(original: Vec<MSBTString>, vec_edited: Vec<Vec<MSBTString>>) -> Vec<MSBTString> {
     let mut result = Vec::<MSBTString>::new();
-    for string in edited {
-        let index = original.iter().position(|s| s.label == string.label);
-        if index.is_none(){
-            result.push(string);
+    for edited in vec_edited{
+        for string in edited {
+            let index = original.iter().position(|s| s.label == string.label);
+            if index.is_none(){
+                result.push(string);
+            }
         }
     }
     return result;
 }
-pub fn get_deleted(original: Vec<MSBTString>, edited: Vec<MSBTString>) -> Vec<MSBTString> {
+pub fn get_deleted(original: Vec<MSBTString>, vec_edited: Vec<Vec<MSBTString>>) -> Vec<MSBTString> {
     let mut result = Vec::<MSBTString>::new();
-    for string in original {
-        let index = edited.iter().position(|s| s.label == string.label);
-        if index.is_none(){
-            result.push(string);
+    for edited in vec_edited{
+        for string in &original {
+            let index = edited.iter().position(|s| s.label == string.label);
+            if index.is_none(){
+                result.push(string.to_owned());
+            }
+        }
+    }
+    return result;
+}
+
+pub fn get_edited(original: Vec<MSBTString>, vec_edited: Vec<Vec<MSBTString>>) -> Vec<MSBTString> {
+    let mut result = Vec::<MSBTString>::new();
+    for edited in vec_edited{
+        for string in edited {
+            let index = original.iter().position(|s| s.label == string.label);
+            if !index.is_none(){
+                let string_original = original.get(index.unwrap()).unwrap();
+                if string_original.string != string.string{
+                    result.push(string);
+                }
+            }
         }
     }
     return result;
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 use std::{
-    collections::HashMap,
-    ffi::OsStr,
-    fs::File,
-    io::{Read, Write},
-    path::Path,
-    string,
+    collections::HashMap, convert, ffi::OsStr, fs::{self, File}, io::{Read, Write}, path::Path, string
 };
 
-use ::msbt::msbt::MSBTString;
+use ::msbt::msbt::{MSBTString, MSBT};
 use bytestream::ByteOrder;
 use clap::{Parser, ValueEnum};
 use msbt::msbt;
 use serde::{Deserialize, Serialize};
 use toml;
+
+mod diff_utils;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -47,7 +44,7 @@ fn main() -> ::msbt::Result<()> {
     match args.action {
         Actions::EXTRACT => return extract_msbt(args),
         Actions::CREATE => return create_msbt(args),
-        Actions::DIFF => todo!(),
+        Actions::DIFF => return diff_msbt(args),
         Actions::PATCH => todo!(),
     }
 }
@@ -100,6 +97,93 @@ fn create_msbt(args: Args) -> ::msbt::Result<()> {
     let new_msbt = msbt::to_binary(strings, order)?;
     let mut result = File::create(filepath.join(filename.to_owned() + ".msbt"))?;
     result.write(&new_msbt)?;
+    Ok(())
+}
+
+fn diff_msbt(args: Args) -> ::msbt::Result<()> {
+    let arg_filename = args.original.clone();
+    let path = Path::new(&arg_filename);
+    let filename = path.file_stem().unwrap().to_str().unwrap();
+    let extension = path.extension().unwrap().to_str().unwrap().to_lowercase();
+
+    //Getting original strings...
+    let mut orig_strings = Vec::<MSBTString>::new();
+    let mut hash;
+    let mut endianness;
+    if extension == "msbt" {
+        let bytes = fs::read(args.original.clone()).unwrap();
+        hash = sha256::digest(&bytes);
+        let mut file = File::open(args.original)?;
+        let msbt = msbt::from_binary(&mut file)?;
+        endianness = msbt.endianness;
+        orig_strings = msbt::get_strings(msbt.clone())?;
+    } else { //Just assume it's toml
+        let file = File::open(args.original)?;
+        let toml = get_toml(file)?;
+        endianness = get_endianness_toml(&toml)?;
+        orig_strings = get_strings_toml(&toml)?;
+        hash = "".to_owned();
+    }
+
+    //Getting edited strings...
+    let mut edited_strings = Vec::<Vec<MSBTString>>::new();
+    for path_edited in args.edited {
+        let mut edited_string_single = Vec::<MSBTString>::new();
+        let arg_filename = path_edited.clone();
+        let path = Path::new(&arg_filename);
+        let extension = path.extension().unwrap().to_str().unwrap().to_lowercase();
+        if extension == "msbt" {
+            let mut file = File::open(path_edited)?;
+            let msbt = msbt::from_binary(&mut file)?;
+            edited_string_single = msbt::get_strings(msbt.clone())?;
+        } else { //Just assume it's toml
+            let file = File::open(path_edited)?;
+            let toml = get_toml(file)?;
+            edited_string_single = get_strings_toml(&toml)?;
+        }
+        edited_strings.push(edited_string_single);
+    }
+    let added_strings = diff_utils::get_added(orig_strings.clone(), edited_strings.clone());
+    let deleted_strings = diff_utils::get_deleted(orig_strings.clone(), edited_strings.clone());
+    let edited_strings = diff_utils::get_edited(orig_strings, edited_strings);
+    let mut diff_file = File::create(filename.to_owned()+".msbd.txt")?;
+
+    //Writing file
+    let _ = diff_file.write((filename.to_owned()+"\n").as_bytes());
+    let _ = diff_file.write((filename.to_owned()+"\n").as_bytes());
+    if hash != ""{
+        let _ = diff_file.write((hash+"\n").as_bytes());
+    }
+    let _ = diff_file.write("\n".as_bytes());
+
+    //Writing added strings...
+    for string in added_strings{
+        let label = "+".to_owned()+&string.label+"\n";
+        let _ = diff_file.write(label.as_bytes());
+        let mut parsed_string = ::msbt::structs::TXT2::parse_binary(string.string, endianness);
+        parsed_string.truncate(parsed_string.len() - 1);
+        parsed_string = parsed_string.replace("\n", "\n>");
+        let _ = diff_file.write((">".to_owned()+&parsed_string+"\n").as_bytes());
+        let _ = diff_file.write("\n".as_bytes());
+    }
+
+    //Writing deleted strings...
+    for string in deleted_strings{
+        let label = "-".to_owned()+&string.label+"\n";
+        let _ = diff_file.write(label.as_bytes());
+        let _ = diff_file.write("\n".as_bytes());
+    }
+
+    //Writing edits...
+    for string in edited_strings{
+        let label = "~".to_owned()+&string.label+"\n";
+        let _ = diff_file.write(label.as_bytes());
+        let mut parsed_string = ::msbt::structs::TXT2::parse_binary(string.string, endianness);
+        parsed_string.truncate(parsed_string.len() - 1);
+        parsed_string = parsed_string.replace("\n", "\n>");
+        let _ = diff_file.write((">".to_owned()+&parsed_string+"\n").as_bytes());
+        let _ = diff_file.write("\n".as_bytes());
+    }
     Ok(())
 }
 
